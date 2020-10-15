32e4ec3af75dfe2ffd8c86408abddf972c28a6f6
2017_aleksandarkn <2017_aleksandarkn@LAB24-15.school.npmg.org>
2020-10-15 18:12:25 +0300

    Class Fraction

--- Fraction/src/edu/school/oop/Fraction.java
+++ Fraction/src/edu/school/oop/Fraction.java
@@ -2,4 +2,137 @@ package edu.school.oop;
 
 public class Fraction {
 
+	private int numerator;
+	private int denominator;
+	
+	public int getNumerator()
+	{
+		
+		return numerator;
+		
+	}
+	
+	public int getDenominator()
+	{
+		
+		return denominator;
+		
+	}
+	
+	public Fraction(int numeratorValue , int denominatorValue)
+	{
+		
+		numerator = numeratorValue;
+		if(denominatorValue != 0)
+		{
+			
+			denominator = denominatorValue;
+			
+		}
+		
+	}
+	
+	public Fraction()
+	{
+		
+		numerator = 0;
+		denominator = 1;
+		
+	}
+	
+	public Fraction(int numeratorValue)
+	{
+		
+		numerator = numeratorValue;
+		denominator = 1;
+		
+	}
+	
+	public int setNumerator(int numeratorValue)
+	{
+		
+		numerator = numeratorValue;
+		return numerator;
+		
+	}
+	
+	public int setDenominator(int denominatorValue)
+	{
+		
+		denominator = denominatorValue;
+		return denominator;
+		
+	}
+	
+	public String toString()
+	{
+		
+		String output = getNumerator() + " / " + getDenominator();
+		return output;
+		
+	}
+	
+	public Fraction add(Fraction f)
+	{
+		
+		Fraction sum = new Fraction(f.getDenominator() * numerator + f.getNumerator() * denominator , denominator * f.getDenominator());
+		return sum;
+		
+	}
+	
+	public Fraction substract(Fraction f)
+	{
+		
+		Fraction sum = new Fraction(f.getDenominator() * numerator - f.getNumerator() * denominator , denominator * f.getDenominator());
+		return sum;
+		
+	}
+	
+	public Fraction multiply(Fraction f)
+	{
+		
+		Fraction sum = new Fraction(f.getNumerator() * numerator  , denominator * f.getDenominator());
+		return sum;
+		
+	}
+	
+	public Fraction divide(Fraction f)
+	{
+		
+		Fraction sum = new Fraction(f.getNumerator() * denominator  , numerator * f.getDenominator());
+		return sum;
+		
+	}
+	
+	public void simplify()
+	{
+		
+		int numeratorValue = numerator;
+		int denominatorValue = denominator;
+		
+		while(numeratorValue != 0 && denominatorValue != 0)
+		{
+			
+			if(numeratorValue > denominatorValue)
+			{
+				
+				numeratorValue = numeratorValue - denominatorValue;
+				
+			}
+			else
+			{
+				
+				denominatorValue = denominatorValue - numeratorValue;
+				
+			}
+			
+			int gcd = denominatorValue + numeratorValue;
+			numerator /= gcd;
+			denominator /= gcd;
+			
+		}
+		
+	}
+	
+	
 }
